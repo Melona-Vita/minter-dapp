e923954b0f8da756b5f62e1a9be783fa7672426c
Melona Vita <8eyedgirl@gmail.com>
2022-03-20 16:17:52 -1000

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,7 +58,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xdF796e7aa46070aCEEBB5be09F8D5B34fEe784b8';
 const TREASURY_ADDRESS = '0xdF796e7aa46070aCEEBB5be09F8D5B34fEe784b8';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 8; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.005; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
